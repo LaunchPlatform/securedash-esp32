eccbcbbe5192745bd0b8baede0e912227e20e678
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-03 03:22:08 -0800

    Send data

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use embedded_svc::{
+    http::{client::Client as HttpClient, Method},
+    io::Write,
+    utils::io,
+};
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::hal::gpio::{Input, InputPin, InterruptType, PinDriver, Pull};
 use esp_idf_svc::hal::modem;
 use esp_idf_svc::hal::prelude::Peripherals;
 use esp_idf_svc::hal::task::block_on;
+use esp_idf_svc::http::client::EspHttpConnection;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::sys::{
     esp, esp_partition_find_first, esp_partition_subtype_t_ESP_PARTITION_SUBTYPE_DATA_FAT,
@@ -71,6 +77,41 @@ async fn connect_wifi(modem: modem::Modem) -> anyhow::Result<AsyncWifi<EspWifi<'
     Ok(wifi)
 }
 
+fn post_chunked_request(client: &mut HttpClient<EspHttpConnection>, data: &[u8]) -> anyhow::Result<()> {
+    // Prepare payload
+
+    // Prepare headers and URL
+    let headers = [("content-type", "text/plain")];
+    let url = "http://httpbin.org/post";
+
+    // Send request
+    let mut request = client.post(url, &headers)?;
+    request.write_all(data)?;
+    request.flush()?;
+    log::info!("-> CHUNKED POST {}", url);
+    let mut response = request.submit()?;
+
+    // Process response
+    let status = response.status();
+    log::info!("<- {}", status);
+    let mut buf = [0u8; 1024];
+    let bytes_read = io::try_read_full(&mut response, &mut buf).map_err(|e| e.0)?;
+    log::info!("Read {} bytes", bytes_read);
+    match std::str::from_utf8(&buf[0..bytes_read]) {
+        Ok(body_string) => log::info!(
+            "Response body (truncated to {} bytes): {:?}",
+            buf.len(),
+            body_string
+        ),
+        Err(e) => log::error!("Error decoding response body: {}", e),
+    };
+
+    // Drain the remaining response bytes
+    while response.read(&mut buf)? > 0 {}
+
+    Ok(())
+}
+
 async fn run_async() -> Result<(), anyhow::Error> {
     log::info!("Hello, world!");
 
@@ -123,6 +164,9 @@ async fn run_async() -> Result<(), anyhow::Error> {
 
     let mut button = PinDriver::input(peripherals.pins.gpio14)?;
     button.set_pull(Pull::Up)?;
+
+    let mut client = HttpClient::wrap(EspHttpConnection::new(&Default::default())?);
+
     loop {
         // Asynchronously wait for GPIO events, allowing other tasks
         // to run, or the core to sleep.
@@ -133,6 +177,7 @@ async fn run_async() -> Result<(), anyhow::Error> {
 
         let contents = fs::read_to_string("/disk/myfile.txt").unwrap_or(String::from("N/A"));
         log::info!("File content: {}", contents);
+        post_chunked_request(&mut client, contents.as_bytes());
 
         esp!(unsafe { tinyusb_msc_storage_unmount() })?;
 
