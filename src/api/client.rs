d5f557928cc6b2cff9c8eec0630d742d3ec749fa
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-04 21:23:05 -0800

    Fix some build issue

--- src/api/client.rs
+++ src/api/client.rs
@@ -4,16 +4,16 @@ use esp_idf_svc::ws::client::{
     EspWebSocketClient, EspWebSocketClientConfig, WebSocketEvent, WebSocketEventType,
 };
 use std::cmp::PartialEq;
-use std::ops::Deref;
 use std::sync::mpsc::{channel, Sender};
 use std::sync::{Arc, Mutex, RwLock};
 
 #[derive(Debug, PartialEq)]
 enum APIError {
     AlreadyConnected,
+    EspIOError { error: EspIOError },
 }
 
-#[derive(Debug)]
+#[derive(Debug,Copy,Clone)]
 enum DesiredState {
     Connected,
     Disconnected,
@@ -43,29 +43,27 @@ pub struct APIClient<'a> {
 
 impl<'a> APIClient<'a> {
     pub fn new(endpoint: String, timeout: time::Duration) -> Self {
-        let state = Arc::new(RwLock::new(APIState {
-            desired_state: DesiredState::Disconnected,
-            connection_state: ConnectionState::Disconnected,
-            ws_config: None,
-            ws_client: None,
-        }));
-        let weak_state = Arc::downgrade(&state);
         Self {
             endpoint,
             timeout,
-            state,
+            state: Arc::new(RwLock::new(APIState {
+                desired_state: DesiredState::Disconnected,
+                connection_state: ConnectionState::Disconnected,
+                ws_config: None,
+                ws_client: None,
+            })),
         }
     }
 
-    pub fn get_desired_state(&self) -> &DesiredState {
-        &self.state.read().unwrap().desired_state
+    pub fn get_desired_state(&self) -> DesiredState {
+        self.state.read().unwrap().desired_state
     }
 
-    pub fn connect(&mut self) -> anyhow::<()> {
+    pub fn connect(&mut self) -> Result<(), APIError> {
         let state = self.state.write().unwrap();
         if state.connection_state != ConnectionState::Disconnected {
             let conn_state = &state.connection_state;
-            log::info!("Already in {conn_state} state, do nothing");
+            log::info!("Already in {conn_state:?} state, do nothing");
             return Err(APIError::AlreadyConnected);
         }
         state.desired_state = DesiredState::Connected;
@@ -79,18 +77,21 @@ impl<'a> APIClient<'a> {
             if let Some(state) = state {
                 state.write().unwrap().handle_event(event);
             }
-        })?);
+        }).map_err(|error| {APIError::EspIOError {error}})?);
         log::info!("Change desired state to Connected");
         Ok(())
     }
 
     pub fn disconnect(&mut self) {
-        self.state.write().unwrap().desired_state = DesiredState::Disconnected;
-        log::info!("Change desired state to Disconnected}")
+        let state = self.state.write().unwrap();
+        state.ws_config = None;
+        state.ws_client = None;
+        state.desired_state = DesiredState::Disconnected;
+        log::info!("Change desired state to Disconnected")
     }
 }
 
-impl APIState {
+impl<'a> APIState<'a> {
     fn handle_event(&mut self, event: &Result<WebSocketEvent, EspIOError>) {
         if let Ok(event) = event {
             match event.event_type {
