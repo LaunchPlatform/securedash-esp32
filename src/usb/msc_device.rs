30b4ddfd41b197660a19d3c177b9378679b3b3ca
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-05 21:58:10 -0800

    Fix all broken stuff

--- src/usb/msc_device.rs
+++ src/usb/msc_device.rs
@@ -47,24 +47,23 @@ impl MSCDevice {
             bail!("Already installed");
         }
         let partition = Some(
-            unsafe { EspPartition::new(&self.base_path) }?.ok_or_else(|| {
+            unsafe { EspPartition::new(&self.partition_label) }?.ok_or_else(|| {
                 anyhow::anyhow!(
-                    "Failed to find partition with label {}",
+                    "Failed to find partition with label {:#?}",
                     self.partition_label
                 )
             })?,
         );
-        let wl_partition = Some(EspWlPartition::new(partition.unwrap()).with_context(|| {
-            format!(
-                "Failed to mount partition {} at {}",
-                self.partition_label, self.base_path
-            )
-        })?);
-
-        self.base_path_c_str = CString::new(self.base_path.as_bytes()).unwrap();
+        let mut wl_partition =
+            Some(EspWlPartition::new(partition.unwrap()).with_context(|| {
+                format!(
+                    "Failed to mount partition {} at {}",
+                    self.partition_label, self.base_path
+                )
+            })?);
 
         let config_spi = tinyusb_msc_spiflash_config_t {
-            wl_handle: self.wl_partition.as_mut().unwrap().handle(),
+            wl_handle: wl_partition.as_mut().unwrap().handle(),
             keep_vfs_fat_mount: true,
             callback_mount_changed: Some(storage_mount_changed_cb),
             callback_premount_changed: Some(storage_mount_changed_cb),
@@ -79,7 +78,8 @@ impl MSCDevice {
         esp!(unsafe { tinyusb_msc_storage_init_spiflash(&config_spi) })
             .with_context(|| "Failed to initialize spiflash")?;
 
-        esp!(unsafe { tinyusb_msc_storage_mount(self.base_path_c_str.as_ptr()) })
+        let base_path_c_str = CString::new(self.base_path.as_bytes()).unwrap();
+        esp!(unsafe { tinyusb_msc_storage_mount(base_path_c_str.as_ptr()) })
             .with_context(|| format!("Failed to mount storage at {}", self.base_path))?;
 
         let tusb_cfg = tinyusb_config_t::default();
@@ -88,6 +88,7 @@ impl MSCDevice {
 
         log::info!("TinyUSB driver installed.");
 
+        self.base_path_c_str = base_path_c_str;
         self.wl_partition = wl_partition;
         Ok(())
     }
