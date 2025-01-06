96234b2e2659632bbd1fc14e5a13aad0c4d3eb20
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-05 21:45:23 -0800

    Use svc partition instead

--- src/usb/msc_device.rs
+++ src/usb/msc_device.rs
@@ -1,21 +1,20 @@
 use anyhow::{bail, Context};
+use esp_idf_svc::handle::RawHandle;
+use esp_idf_svc::partition::{EspPartition, EspWlPartition};
 use esp_idf_svc::sys::{
-    esp, esp_partition_find_first, esp_partition_subtype_t_ESP_PARTITION_SUBTYPE_DATA_FAT,
-    esp_partition_type_t_ESP_PARTITION_TYPE_DATA, esp_vfs_fat_mount_config_t, tinyusb_config_t,
-    tinyusb_driver_install, tinyusb_msc_event_t, tinyusb_msc_event_type_t,
-    tinyusb_msc_event_type_t_TINYUSB_MSC_EVENT_MOUNT_CHANGED,
+    esp, esp_vfs_fat_mount_config_t, tinyusb_config_t, tinyusb_driver_install, tinyusb_msc_event_t,
+    tinyusb_msc_event_type_t, tinyusb_msc_event_type_t_TINYUSB_MSC_EVENT_MOUNT_CHANGED,
     tinyusb_msc_event_type_t_TINYUSB_MSC_EVENT_PREMOUNT_CHANGED, tinyusb_msc_spiflash_config_t,
-    tinyusb_msc_storage_init_spiflash, tinyusb_msc_storage_mount, wl_handle_t, wl_mount,
+    tinyusb_msc_storage_init_spiflash, tinyusb_msc_storage_mount,
 };
 use std::ffi::CString;
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct MSCDevice {
     pub partition_label: String,
     pub base_path: String,
-    wl_handle: wl_handle_t,
     base_path_c_str: CString,
-    partition_label_c_str: CString,
+    wl_partition: Option<EspWlPartition<EspPartition>>,
 }
 
 fn msc_event_type_to_str(event_type: tinyusb_msc_event_type_t) -> String {
@@ -44,31 +43,28 @@ impl MSCDevice {
     }
 
     pub fn install(&mut self) -> anyhow::Result<()> {
-        self.partition_label_c_str = CString::new(self.partition_label.as_bytes()).unwrap();
-        self.base_path_c_str = CString::new(self.base_path.as_bytes()).unwrap();
-
-        let data_partition = unsafe {
-            esp_partition_find_first(
-                esp_partition_type_t_ESP_PARTITION_TYPE_DATA,
-                esp_partition_subtype_t_ESP_PARTITION_SUBTYPE_DATA_FAT,
-                self.partition_label_c_str.as_ptr(),
-            )
-        };
-        if data_partition.is_null() {
-            bail!(
-                "Failed to find partition with label {}",
-                self.partition_label
-            );
+        if self.wl_partition.is_some() {
+            bail!("Already installed");
         }
-        esp!(unsafe { wl_mount(data_partition, &mut self.wl_handle) }).with_context(|| {
+        let partition = Some(
+            unsafe { EspPartition::new(&self.base_path) }?.ok_or_else(|| {
+                anyhow::anyhow!(
+                    "Failed to find partition with label {}",
+                    self.partition_label
+                )
+            })?,
+        );
+        let wl_partition = Some(EspWlPartition::new(partition.unwrap()).with_context(|| {
             format!(
                 "Failed to mount partition {} at {}",
                 self.partition_label, self.base_path
             )
-        })?;
+        })?);
+
+        self.base_path_c_str = CString::new(self.base_path.as_bytes()).unwrap();
 
         let config_spi = tinyusb_msc_spiflash_config_t {
-            wl_handle: self.wl_handle,
+            wl_handle: self.wl_partition.as_mut().unwrap().handle(),
             keep_vfs_fat_mount: true,
             callback_mount_changed: Some(storage_mount_changed_cb),
             callback_premount_changed: Some(storage_mount_changed_cb),
@@ -92,6 +88,7 @@ impl MSCDevice {
 
         log::info!("TinyUSB driver installed.");
 
+        self.wl_partition = wl_partition;
         Ok(())
     }
 }
