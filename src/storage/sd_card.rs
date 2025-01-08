35517260e29ed198a4f9b5e50139e3cc1e60bdcf
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-07 17:43:25 -0800

    Fix more build errors

--- src/storage/sd_card.rs
+++ src/storage/sd_card.rs
@@ -10,10 +10,10 @@ use esp_idf_svc::sys::{esp, ff_diskio_get_drive};
 
 pub struct SDCardStorage<'a> {
     sd_card_driver: Option<SdCardDriver<SdMmcHostDriver<'a>>>,
-    mounted_fatfs: Option<MountedFatfs<Fatfs<()>>>,
+    mounted_fatfs: Option<MountedFatfs<Fatfs<SdCardDriver<SdMmcHostDriver<'a>>>>>,
 }
 
-impl SDCardStorage {
+impl<'a> SDCardStorage<'a> {
     pub fn new() -> Self {
         Self {
             sd_card_driver: None,
@@ -21,6 +21,12 @@ impl SDCardStorage {
         }
     }
     pub fn install_driver(&mut self, peripherals: &mut Peripherals) -> anyhow::Result<()> {
+        if self.mounted_fatfs.is_some() {
+            bail!("File system already mounted");
+        }
+        if self.sd_card_driver.is_some() {
+            bail!("Driver already installed");
+        }
         let pins = &mut peripherals.pins;
         self.sd_card_driver = Some(SdCardDriver::new_mmc(
             SdMmcHostDriver::new_4bits(
@@ -41,15 +47,16 @@ impl SDCardStorage {
     }
 
     pub fn mount(&mut self, mount_path: &str, max_fds: usize) -> anyhow::Result<()> {
-        if self.sd_card_driver.is_none() {
-            bail!("SD card driver not installed yet");
-        }
         if self.mounted_fatfs.is_some() {
             bail!("File system already mounted");
+        } else {
+            if self.sd_card_driver.is_none() {
+                bail!("SD card driver not installed yet");
+            }
         }
         let mut drive: u8 = 0;
         esp!(unsafe { ff_diskio_get_drive(&mut drive) })?;
-        let fatfs = unsafe { Fatfs::new_sdcard(drive, self.handle()) }?;
+        let fatfs = unsafe { Fatfs::new_sdcard(drive, self.sd_card_driver.take().unwrap()) }?;
         self.mounted_fatfs = Some(MountedFatfs::mount(fatfs, mount_path, max_fds)?);
         Ok(())
     }
