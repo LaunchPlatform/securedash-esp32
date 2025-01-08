34ea185acf409aff89859e76a9f8a7624b8fde29
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-07 17:10:29 -0800

    Remove todos

--- src/storage/spiflash.rs
+++ src/storage/spiflash.rs
@@ -50,12 +50,6 @@ impl SPIFlashStorage {
         let mut drive: u8 = 0;
         esp!(unsafe { ff_diskio_get_drive(&mut drive) })?;
         let fatfs = unsafe { Fatfs::new_wl_part(drive, self.handle()) }?;
-        // TODO: handle file system not formatted issue.
-        //       looks like esp-idf-svc didn't implement error handling at this moment:
-        //       https://github.com/esp-rs/esp-idf-svc/blob/6453bedd5967ebc3bcb7d240492a0d5164e818c2/src/io.rs#L130
-        // TODO: the f_mount called with async mode, it means the actual mount will only happen in
-        //       a file operation. Maybe we should leave file sys formatting to the end-user
-        //       instead? as it's a dangerous operation anyway....
         self.mounted_fatfs = Some(MountedFatfs::mount(fatfs, mount_path, max_fds)?);
         Ok(())
     }
