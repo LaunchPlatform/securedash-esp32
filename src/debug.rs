f8168ae08c8ec0b1db39d7cd3aa7be4f75a06d28
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-09 12:32:48 -0800

    More debug info for card

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,4 @@
-use esp_idf_svc::sys::{sdmmc_card_t, sdmmc_host_t};
+use esp_idf_svc::sys::{sdmmc_card_t, sdmmc_csd_t, sdmmc_ext_csd_t, sdmmc_host_t};
 use std::fmt;
 use std::fmt::{Debug, Formatter};
 
@@ -31,12 +31,55 @@ impl Debug for HostInfo<'_> {
             .finish()
     }
 }
+pub struct CSDInfo<'a> {
+    csd: &'a sdmmc_csd_t,
+}
+impl<'a> CSDInfo<'a> {
+    fn new(csd: &'a sdmmc_csd_t) -> CSDInfo<'a> {
+        Self { csd }
+    }
+}
+impl Debug for CSDInfo<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("sdmmc_csd_t")
+            .field("csd_ver", &self.csd.csd_ver)
+            .field("mmc_ver", &self.csd.mmc_ver)
+            .field("capacity", &self.csd.capacity)
+            .field("sector_size", &self.csd.sector_size)
+            .field("read_block_len", &self.csd.read_block_len)
+            .field("card_command_class", &self.csd.card_command_class)
+            .field("tr_speed", &self.csd.tr_speed)
+            .finish()
+    }
+}
+
+pub struct ExtCSDInfo<'a> {
+    ext_csd: &'a sdmmc_ext_csd_t,
+}
+impl<'a> ExtCSDInfo<'a> {
+    fn new(ext_csd: &'a sdmmc_ext_csd_t) -> ExtCSDInfo<'a> {
+        Self { ext_csd }
+    }
+}
+impl Debug for ExtCSDInfo<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("sdmmc_ext_csd_t")
+            .field("rev", &self.ext_csd.rev)
+            .field("power_class", &self.ext_csd.power_class)
+            .field("erase_mem_state", &self.ext_csd.erase_mem_state)
+            .field("sec_feature", &self.ext_csd.sec_feature)
+            .finish()
+    }
+}
 
 impl Debug for CardInfo<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.debug_struct("sdmmc_card_t")
             .field("host", &HostInfo::new(&self.card.host))
+            .field("csd", &CSDInfo::new(&self.card.csd))
+            .field("ext_csd", &ExtCSDInfo::new(&self.card.ext_csd))
             .field("ocr", &self.card.ocr)
+            .field("rca", &self.card.rca)
             .field("max_freq_khz", &self.card.max_freq_khz)
             .field("real_freq_khz", &self.card.real_freq_khz)
             .finish()
